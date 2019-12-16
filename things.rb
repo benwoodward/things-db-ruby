80716c847babaad84abda5bcf6967eb574950367
Ben Woodward <benwoodward@users.noreply.github.com>
2019-12-16 11:56:33 +1100

    Add notes + things URL to each Sorted task

--- things.rb
+++ things.rb
@@ -16,7 +16,7 @@ MAX_MINUTES=8*60
 def duration_in_minutes(tags)
   dur_tag = tags.select {|tag| tag.title =~ /dur:/}.first
   if dur_tag.nil?
-    return "15"
+    return 15
   else
     extract_minutes_from_dur_string(dur_tag.title)
   end
@@ -58,7 +58,7 @@ class Tag < Sequel::Model(DB[:TMTag])
 end
 
 def contains_urgent_tag?(tags)
-  tags.select {|tag| tag.title == 'URGENT!'}.count > 0
+  tags.select {|tag| tag.title == 'imp:URGENT!'}.count > 0
 end
 
 def contains_high_imp_tag?(tags)
@@ -69,7 +69,7 @@ def contains_med_imp_tag?(tags)
   tags.select {|tag| tag.title == 'imp:medium'}.count > 0
 end
 
-def reprioritise(tasks)
+def order_by_importance(tasks)
   [:contains_med_imp_tag?, :contains_high_imp_tag?, :contains_urgent_tag?].inject(tasks) do |reordered_tasks, filter_method|
     reordered_tasks = apply_filter(reordered_tasks, filter_method)
   end
@@ -77,27 +77,39 @@ end
 
 def apply_filter(tasks, filter_method)
   tasks.partition do |task|
-    send filter_method, task.tags
+    self.send filter_method, task.tags
   end.flatten
+end
+
+def task_content(task)
+  newline_char = "\n"
+  "#{task[:title]}#{newline_char}#{things_url(task[:uuid])}#{newline_char}#{task[:notes]}"
 end
 
 tasks = Task.eager(:tags)
   .where(trashed: 0, status: 0, type: 0, start: 1)
   .where(Sequel.~(startdate: nil))
   .order(:todayIndex)
-  .limit(10)
+  .limit(100)
 
-reprioritised_tasks = reprioritise(tasks)
+def sorted3_order(tasks)
+  first_item = [tasks[0]]
+  reversed_list = tasks.drop(1).reverse
+  first_item + reversed_list
+end
+
+ordered_by_importance = order_by_importance(tasks)
+reordered_for_sorted_position_2_bug = sorted3_order(ordered_by_importance)
 
 todays_tasks = []
 combined_duration = 0
 
-reprioritised_tasks.each do |task|
+reordered_for_sorted_position_2_bug.each do |task|
   duration = duration_in_minutes(task.tags)
 
   task = {
     things_url: things_url(task[:uuid]),
-    title: task[:title],
+    content: task_content(task),
     duration: duration
   }
 
@@ -111,7 +123,10 @@ end
 client = Octokit::Client.new(:access_token => GITHUB_THINGS_TOKEN)
 
 output = todays_tasks.join(',')
-puts output
+
+ordered_by_importance.each do |task|
+  puts task.title
+end
 
 client.edit_gist(GIST_ID, {
   files: {"todays_tasks.json" => {content: "[#{output}]"}}
